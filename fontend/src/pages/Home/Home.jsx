51088d60c2f0245eb4a5e335e221d6ae9eca419b
cuongbv-11 <vc13050904@gmail.com>
2024-06-30 05:51:04 +0700

    add

--- fontend/src/pages/Home/Home.jsx
+++ fontend/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../commponets/Header/Header";
+import ExploreMenu from "../../commponets/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../commponets/FoodDisplay/FoodDisplay";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   );
 };
